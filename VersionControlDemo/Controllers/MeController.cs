b3f1643d7561c63d5526e17d94c7bad19b6571e0
Han Nguyen <baohanyumi2727@gmail.com>
2025-11-02 20:47:38 -0500

    Initial commit

---
+++ VersionControlDemo/Controllers/MeController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace VersionControlDemo.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class MeController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok(new { name = "Han Nguyen" });
+        }
+    }
+}
